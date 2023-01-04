357b7767b4248e5e8c64ecda0cb4bced74606122
jaka2005 <jakalogin1808@gmail.com>
2023-01-04 22:50:53 +0400

    rename project

--- src/main/java/com/j2k/worldbox/WorldBoxMod.java
+++ src/main/java/com/j2k/worldbox/WorldBoxMod.java
@@ -1,10 +1,8 @@
-package com.j2k.testmod;
+package com.j2k.worldbox;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -17,17 +15,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TestMod.MOD_ID)
-public class TestMod
+@Mod(WorldBoxMod.MOD_ID)
+public class WorldBoxMod
 {
-    public static  final String MOD_ID = "testmod";
+    public static  final String MOD_ID = "worldbox";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public TestMod() {
+    public WorldBoxMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
