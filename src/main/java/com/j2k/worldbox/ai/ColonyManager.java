89ab5c2506bb9c108bb2cfb75157f407b5bb1d43
Shelbyen <cool.sheberstov@mail.ru>
2023-02-15 02:18:31 +0500

    small code refactoring and start working on civilian logic

--- src/main/java/com/j2k/worldbox/ai/ColonyManager.java
+++ src/main/java/com/j2k/worldbox/ai/ColonyManager.java
@@ -3,19 +3,22 @@ package com.j2k.worldbox.ai;
 import com.j2k.worldbox.buildings.Building;
 import com.j2k.worldbox.entity.custom.CivilianEntity;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ColonyManager {
-
-    private final HashMap<String, CivilianEntity> colonists;
+    public static final int MINIMAL_COLONY_RADIUS = 10;
+    private final HashMap<String, ArrayList<CivilianEntity>> colonists;
     private final HashMap<String, Building> buildings;
 
-    public ColonyManager(HashMap<String, CivilianEntity> colonists, HashMap<String, Building> buildings) {
+    // TODO: Change parameters to List and made distribution villagers
+    public ColonyManager(HashMap<String, ArrayList<CivilianEntity>> colonists, HashMap<String, Building> buildings) {
         this.colonists = colonists;
         this.buildings = buildings;
     }
 
-    public HashMap<String, CivilianEntity> getColonists() {
+    public HashMap<String, List<CivilianEntity>> getColonists() {
         return new HashMap<>(this.colonists);
     }
 
--- src/main/java/com/j2k/worldbox/buildings/Building.java
+++ src/main/java/com/j2k/worldbox/buildings/Building.java
@@ -1,10 +1,31 @@
 package com.j2k.worldbox.buildings;
 
 import com.j2k.worldbox.entity.custom.CivilianEntity;
+import net.minecraft.util.math.BlockPos;
 
 import java.util.ArrayList;
 
 public abstract class Building {
+    private final BlockPos position;
     private int state;
     private ArrayList<CivilianEntity> builders;
+    private ArrayList<CivilianEntity> using;
+
+    protected Building(BlockPos position, ArrayList<CivilianEntity> builders) {
+        this.position = position;
+        this.builders = builders;
+    }
+
+    public ArrayList<CivilianEntity> getBuilders() {
+        return new ArrayList<>(this.builders);
+    }
+
+    public void addBuilders(CivilianEntity newBuilder) {
+        this.builders.add(newBuilder);
+    }
+
+    protected ArrayList<CivilianEntity> removeBuilders() {
+        this.builders.clear();
+        return null;
+    }
 }
